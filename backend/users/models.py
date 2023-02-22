643d0c6b07cdf5e04c7ffa386cd4e0de1fd0ac48
Stephen Oba <obastepheno@gmail.com>
2023-02-22 23:52:44 +0100

    Add profile table

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,15 +1,33 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
     """
-    Default custom user model for Sample Project.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
+    Default custom user model
     """
-
     #: First and last name do not cover name patterns around the globe
-    name = CharField("Name of User", blank=True, max_length=255)
+    name = models.CharField("Name of User", blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+
+
+class Profile(models.Model):
+    """
+    Extra information about user
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    prog_lang = models.CharField("Programming Language", blank=True, null=True, max_length=20)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
